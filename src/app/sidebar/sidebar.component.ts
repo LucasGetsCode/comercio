4ac1069527df652cb31d51c5e6ec32b2d552a801
Nicolás Pujía <nicopujia.dev@gmail.com>
2023-12-29 18:44:34 -0300

    Sidebar estilosa

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output, Input } from '@angular/core';
+
+export interface Carpeta {
+  nombre: string;
+  id: number;
+  color_pred: string;
+}
 
 @Component({
   selector: 'app-sidebar',
@@ -6,5 +12,34 @@ import { Component } from '@angular/core';
   styleUrl: './sidebar.component.css'
 })
 export class SidebarComponent {
+  sidebarOpen: boolean = false;
+  creando: boolean = false;
 
+  @Input() carpetas: Carpeta[] = [];
+
+  @Output() toggle = new EventEmitter();
+  @Output() carpeta_select = new EventEmitter();
+  @Output() carpeta_create = new EventEmitter();
+
+  cambio_carpeta(carpeta: Carpeta) {
+    this.carpeta_select.emit(carpeta.id);
+  }
+
+  cancelar_carpeta() {
+    this.creando = false;
+  }
+
+  creando_carpeta() {
+    this.creando = true;
+  }
+
+  crear_carpeta([nombre, color_pred]: [string, string]) {
+    this.carpeta_create.emit([nombre, color_pred]);
+    this.creando = false;
+  }
+
+  toggleSidebar() {
+    this.sidebarOpen = !this.sidebarOpen;
+    this.toggle.emit(this.sidebarOpen);
+  }
 }
